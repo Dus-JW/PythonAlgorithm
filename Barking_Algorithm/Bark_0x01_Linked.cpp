c87fd3dc83bf7beecd60edd4cf4b137114f30f60
jaewoopk <jaewoopk000@naver.com>
2021-12-16 22:10:08 +0900

    Day 8 Try Linked list to add & erase

--- Barking_Algorithm/Bark_0x01_Linked.cpp
+++ Barking_Algorithm/Bark_0x01_Linked.cpp
@@ -3,17 +3,46 @@ using namespace std;
 
 typedef struct sNode
 {
-	struct sNode	*prev;
-	struct sNode	*next;
-	int				data;
+	struct sNode    *prev;
+	struct sNode    *next;
+	int             data;
 } tNode;
+
+void	ft_erase(tNode &tlist, int ans)
+{
+	int		i;
+
+	i = 0;
+	while (1)
+	{
+		if (tlist.data == ans)
+		{
+			i = 1;
+			break ;
+		}
+		tlist = *(tlist.next);
+	}
+	if (i == 1)
+	{
+		tlist.prev = tlist.next;
+		(tlist.next)->prev = tlist.prev;
+	}
+}
 
 int main(void)
 {
-	tNode a,b;
+	tNode a,b,c;
 	a.next = &b;
+	b.prev = &a;
+	b.next = &c;
+	c.prev = &b;
+	c.next = NULL;
 	a.data = 10;
 	b.data = 20;
-	cout << a.data << (a.next)->data << endl;
+	c.data = 30;
+
+	cout << a.data << ' ' << (a.next)->data << ' ' << (b.next)->data << endl;
+	ft_erase(a, 20);
+	cout << (a.next)->data << endl;
 	return (0);
 }
