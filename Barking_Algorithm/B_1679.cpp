a1eb47908a52db4dc34990fb060d1f81abc4dd02
jaewoopk <jaewoopk000@naver.com>
2022-01-12 15:45:44 +0900

    Day 22 B_1679 solving

---
+++ Barking_Algorithm/B_1679.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n,m;
+int	count;
+
+int main(void)
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cin >> n >> m;
+	while (n != m)
+	{
+		if (m / 2 > n)
+			n *= 2;
+		else if (n > m)
+	}
+	return (0);
+}
