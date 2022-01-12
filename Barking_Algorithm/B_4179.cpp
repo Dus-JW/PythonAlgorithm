3d88843a29ccc1851c1796e70fecdf914f637931
jaewoopk <jaewoopk000@naver.com>
2022-01-12 14:55:17 +0900

    Day 22 B_4179 not done

--- Barking_Algorithm/B_4179.cpp
+++ Barking_Algorithm/B_4179.cpp
@@ -4,47 +4,36 @@ using namespace std;
 # define X first
 # define Y second
 
-int		n,m;
-string	board[1001][1001];
-string	dist[1001][1001];
+int			n,m;
+string		board[1001];
+string		dist[1001];
+int			real_max;
 
 int dx[4] = {1, 0, -1, 0};
 int dy[4] = {0, 1, 0, -1};
 
-void	ft_dist_print()
+void	ft_BFS(queue<pair<char,char> > &JQ, queue<pair<char,char> > &FQ, int n, int m)
 {
-	for (int i = 0; i < n; i++)
+	while (!FQ.empty())
 	{
-		for (int j = 0; j < m; j++)
-		{
-			cout << dist[i][j] << " ";
-		}
-		cout << '\n';
-	}
-}
-
-
-void	ft_BFS(queue<pair<int,int> > &JQ, queue<pair<int,int> > &FQ, int n, int m)
-{
-	int	count = 0;
-
-	while (!JQ.empty())
-	{
-		pair<int,int> cur1 = JQ.front();
-		pair<int,int> cur2 = FQ.front();
-		JQ.pop();
+		pair<char,char> cur2 = FQ.front();
 		FQ.pop();
 		for (int dir = 0; dir < 4; dir++)
 		{
 			int nx = cur2.X + dx[dir];
 			int ny = cur2.Y + dy[dir];
 			if (nx < 0 || nx >= n || ny < 0 || ny >= m)
-				continue ;
-			if (dist[nx][ny] >= 1 || dist[nx][ny] == -1)
-				continue ;
-			dist[nx][ny] = dist[cur2.X][cur2.Y] + 1;
-			FQ.push(make_pair(nx,ny));
+                continue ;
+            if (dist[nx][ny] >= 1 || dist[nx][ny] == -1)
+                continue ; 
+            dist[nx][ny] = dist[cur2.X][cur2.Y] + 1;
+            FQ.push(make_pair(nx,ny));
 		}
+	}
+	while (!JQ.empty())
+	{
+		pair<char,char> cur1 = JQ.front();
+		JQ.pop();
 		for (int dir = 0; dir < 4; dir++)
 		{
 			int nx = cur1.X + dx[dir];
@@ -63,13 +52,14 @@ int main(void)
 {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
-	queue<pair<int,int> > JQ;
-	queue<pair<int,int> > FQ;
+	queue<pair<char,char> > JQ;
+	queue<pair<char,char> > FQ;
 
 	cin >> n >> m;
+	getchar();
 	for (int i = 0; i < n; i++)
 	{
-		cin.getline(board[i]);
+		getline(cin, board[i]);
 		for (int j = 0; j < m; j++)
 		{
 			if (board[i][j] == 'J')
@@ -92,8 +82,20 @@ int main(void)
 			}
 		}
 	}
-	ft_dist_print();
 	ft_BFS(JQ, FQ, n, m);
-	ft_dist_print();
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < m; j++)
+		{
+			if (dist[i][j] == 0)
+			{
+				cout << "IMPOSSIBLE\n";
+				return (0);
+			}
+			if (dist[i][j] > real_max)
+				real_max = dist[i][j];
+		}
+	}
+	cout << real_max - 1;
 	return (0);
 }
