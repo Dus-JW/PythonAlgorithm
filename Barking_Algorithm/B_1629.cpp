03dbafd5434fd7425cc7344fc241c5d3aa9f01a1
jaewoopk <jaewoopk000@naver.com>
2022-01-18 15:14:01 +0900

    Day 27 recursive B_1629 understand

--- Barking_Algorithm/B_1629.cpp
+++ Barking_Algorithm/B_1629.cpp
@@ -3,15 +3,10 @@ using namespace std;
 
 int recursive(int a, int b, int c)
 {
-	cout << a <<"  " << b << "  " << a % c << endl;
 	if (b == 1)
 		return (a % c);
 	long long val = recursive(a, b/2, c);
-	cout << "first val == " << val << endl;
 	val = val * val % c;
-	cout << "second val == " << val << endl;
-	cout << "it's return value == " << val * a % c << endl;
-	cout << "b is == " << b << endl;
 	if (b%2==0) return (val);
 	return (val * a % c);
 }
