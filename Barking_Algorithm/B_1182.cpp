163bfbe0345db5eb16aa66ba43377e9800112ac7
Dus_JW <jaewoopk000@naver.com>
2022-01-25 20:16:00 +0900

    Day 32 Trying BackTracking but it's not easy

--- Barking_Algorithm/B_1182.cpp
+++ Barking_Algorithm/B_1182.cpp
@@ -2,27 +2,34 @@
 #include <algorithm>
 using namespace std;
 
-int		n,s,t;
+int		n,s,m =5;
 int		store[21];
 int		arr[21];
-bool	isused[21];
+bool	issued[21];
 
 void func(int k)
 {
-	cout << "k == " << k << endl;
-	if (k == n)
+	if (k == m)
 	{
+		for (int i = 0; i < m; i++)
+		{
+			cout << arr[i] << ' ';
+		}
+		cout << '\n';
 		return ;
 	}
-	for (int i = t; i < k; i++)
+
+	for (int i = 0; i <= n; i++)
 	{
-		isused[i] = 1;
-		if (!isused[k])
-			func(k + 1);
-		cout << store[i] << " ";
-		isused[k] = 0;
+		if (!(issued[i]))
+		{
+			arr[k] = store[i];
+			issued[i] = 1;
+			if (k == 0 || arr[k - 1] < i)
+				func(k+1);
+			issued[i] = 0;
+		}
 	}
-	cout << '\n';
 }
 
 int main(void)
@@ -31,8 +38,9 @@ int main(void)
 	cin.tie(0);
 
 	cin >> n >> s;
+	getchar();
 	for (int i = 0; i < n; i++)
 		cin >> store[i];
-	func(1);
+	func(0);
 	return (0);
 }
