8afaf0d880975aeaa143c597a5f6dac5e7106e2a
Dus_JW <jaewoopk000@naver.com>
2022-01-25 19:09:18 +0900

    Day 32 I Solved N_Queen Only my knowledge

--- Barking_Algorithm/B_9663.cpp
+++ Barking_Algorithm/B_9663.cpp
@@ -7,15 +7,20 @@ int		cnt;
 int		arr[31][31];
 bool	issued[31][31];
 bool	i_visit[31][31];
-int		dist1[4] = {-1, 1, -1 ,1};
-int		dist2[4] = {-1, -1, 1, 1};
+int		dist1[4] = {-1, -1, 1 ,1};
+int		dist2[4] = {-1, 1, -1, 1};
 bool	find_wrong = false;
 
-bool	is_possible_col_row(int col)
+bool	is_possible_col_row(int col, int row)
 {
 	for (int i = 0; i < n; i++)
 	{
-		if (issued[col][i] == 1 || issued[i][col] == 1)
+		if (issued[col][i] == 1)
+			return (false);
+	}
+	for (int i = 0; i < n; i++)
+	{
+		if (issued[i][row] == 1)
 			return (false);
 	}
 	return (true);
@@ -30,7 +35,6 @@ bool	is_possible_diag(int col, int row, int x)
 		d2 = dist2[i];
 		for (int j = 0; j < n; j++)
 		{
-			cout << "col : " << col << ", row :" << row << "it 's d1 d2 :" << d1 << ' ' << d2 << endl;
 			if (col + d1 > n || col + d1 < 0 || row + d2 > n || row + d2 < 0)
 				break ;
 			else if (issued[col+ d1][row + d2] == 1)
@@ -53,39 +57,18 @@ bool	is_possible_diag(int col, int row, int x)
 
 void	func(int k)
 {
-	cout << k << endl;
 	if (k == n)
 	{
-		for (int i = 0; i < n; i++)
-		{
-			for (int j = 0; j < n; j++)
-			{
-				cout << arr[i][j] << ' ';
-			}
-			cout << '\n';
-		}
-		cout << "-------\n"; 
 		cnt++;
 		return ;
 	}
 	for (int j = 0; j < n; j++)
 	{
-		cout << "---------------" << k << ' ' << j << "------------------\n"; 
 		find_wrong = true;
-		if (is_possible_col_row(j) && is_possible_diag(k,j,0))
+		if (is_possible_col_row(k,j) && is_possible_diag(k,j,0))
 		{
-			cout << k << ' ' << j << " : " << k << endl;
 			arr[k][j] = 1;
 			issued[k][j] = 1;
-			for (int i = 0; i < n; i++)
-			{
-				for (int j = 0; j < n; j++)
-				{
-					cout << arr[i][j] << ' ';
-				}
-				cout << '\n';
-			}
-			cout << "-------\n";
 			func(k + 1);
 			arr[k][j] = 0;
 			issued[k][j] = 0;
@@ -100,5 +83,6 @@ int main(void)
 
 	cin >> n;
 	func(0);
+	cout << cnt;
 	return (0);
 }
