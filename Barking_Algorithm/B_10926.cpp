b30bd9c80046e3fb232cf6f6d4b151a45b0b9283
jaewoopk <jaewoopk000@naver.com>
2022-01-27 15:16:51 +0900

    Day 33 -

---
+++ Barking_Algorithm/B_10926.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void)
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	string name;
+	cin >> name;
+	cout << name << "\?\?!";
+	return (0);
+}
