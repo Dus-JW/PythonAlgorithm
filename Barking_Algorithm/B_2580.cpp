36642459757e81f1dc9031e6d9fde72893df20d6
jaewoopk <jaewoopk000@naver.com>
2022-01-30 02:07:51 +0900

    Day 36 Trying sudoku it's fun

---
+++ Barking_Algorithm/B_2580.cpp
@@ -0,0 +1,73 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int		board[9][9];
+bool	isused[9][9];
+
+bool	is_right(int col, int row)
+{
+	for (int i = 0; i < 9; i++)
+	{
+		if (row == i)
+			continue ;
+		else if (board[col][i] == board[col][row])
+			return (false);
+	}
+	for (int i = 0; i < 9; i++)
+	{
+		if (col == i)
+			continue ;
+		else if (board[i][row] == board[col][row])
+			return (false);
+	}
+	return (true);
+}
+
+void	sudoku(int k)
+{
+	int t = 0;
+	if (k == 9)
+	{
+		for (int i = 0; i < 9; i++)
+		{
+			for (int j = 0; j < 9; j++)
+			{
+				cout << board[i][j] << ' ';
+			}
+			cout << '\n';
+		}
+		cout << "--------------------\n";
+		return ;
+	}
+	for (int i = 0; i < 9; i++)
+	{
+		if (!isused[k][i])
+		{
+			cout << k << ' ' << i << '\n';
+			board[k][i] = i + 1;
+			isused[k][i] = true;
+			if (is_right(k,i))
+				sudoku(k + 1);
+			board[k][i] = 0;
+			isused[k][i] = false;
+		}
+	}
+}
+
+int main(void)
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	for (int i = 0; i < 9; i++)
+	{
+		for (int j = 0; j < 9; j++)
+		{
+			cin >> board[i][j];
+			if (board[i][j] != 0)
+				isused[i][j] = true;
+		}
+	}
+	sudoku(0);
+	return (0);
+}
